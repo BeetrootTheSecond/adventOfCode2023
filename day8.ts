8cbe0e8f2ede6fb123437fb8977e2e4d8b0cb4e1
Aaron Horrell <aaronhorrell@hotmail.co.uk>
2023-12-11 11:07:14 +0000

    Day 8 star Two Complete

--- day8.ts
+++ day8.ts
@@ -6,117 +6,142 @@ const currentData = sampleMode ? sample3 : data;
 const STARONE = false;
 
 interface nodemapMap {
-    [x: string]: nodemapNode;
+  [x: string]: nodemapNode;
 }
 interface nodemapNode {
-    left: string,
-    right: string,
-    [key: string]: any;
+  left: string;
+  right: string;
+  [key: string]: any;
 }
 
 // split the data by \n\n
 
-let splitData = currentData.split('\n\n');
+let splitData = currentData.split("\n\n");
 
 // to get navigation LR and node map
-let navigation: string[] = splitData[0].split('');
-let nodeMapString = splitData[1].split('\n').map(row => {
+let navigation: string[] = splitData[0].split("");
+let nodeMapString = splitData[1].split("\n").map((row) => {
+  let rowSplit = row.split(" = ");
+  let directionsSplit = rowSplit[1].split(",");
 
-    let rowSplit = row.split(' = ');
-    let directionsSplit = rowSplit[1].split(',');
-
-
-    return {
-        nodeName: rowSplit[0],
-        left: directionsSplit[0].replace('(', '').trim(),
-        right: directionsSplit[1].replace(')', '').trim()
-
-    }
-})
+  return {
+    nodeName: rowSplit[0],
+    left: directionsSplit[0].replace("(", "").trim(),
+    right: directionsSplit[1].replace(")", "").trim(),
+  };
+});
 
 //console.table(navigation);
 //console.table(nodeMapString);
 //parse node map into object
 let nodeMap: nodemapMap = {};
 
-nodeMapString.map(node => {
-    nodeMap[node.nodeName] = {
-        left: node.left,
-        right: node.right
-    }
-})
+nodeMapString.map((node) => {
+  nodeMap[node.nodeName] = {
+    left: node.left,
+    right: node.right,
+  };
+});
 
 console.table(nodeMap);
 
-// traveral node map to ZZZ 
-let startingNode = 'AAA';
-let endNode = 'ZZZ';
+// traveral node map to ZZZ
+let startingNode = "AAA";
+let endNode = "ZZZ";
 let currentNode = startingNode;
 let navigationIndex = 0;
 let traversingSteps = [];
 
 if (STARONE) {
-    while (currentNode != endNode) {
-        traversingSteps.push(currentNode);
-        let node = nodeMap[currentNode];
+  while (currentNode != endNode) {
+    traversingSteps.push(currentNode);
+    let node = nodeMap[currentNode];
 
-        currentNode = node.left;
-        if (navigation[navigationIndex] == 'R') {
-            currentNode = node.right;
-        }
-
-        navigationIndex++;
-        if (navigation.length == navigationIndex) {
-            navigationIndex = 0;
-        }
-
+    currentNode = node.left;
+    if (navigation[navigationIndex] == "R") {
+      currentNode = node.right;
     }
 
-    console.log(`StarOne : ${traversingSteps.length}`);
+    navigationIndex++;
+    if (navigation.length == navigationIndex) {
+      navigationIndex = 0;
+    }
+  }
+
+  console.log(`StarOne : ${traversingSteps.length}`);
 }
 
 let traversingStepsCount = 0;
 // star Two
 //find all rows which end with 'A'
 if (!STARONE) {
+  //let nodeMapSet = new Set(nodeMap);
+  let startingNodes = Object.keys(nodeMap).filter(
+    (nodeName) => [...nodeName][2] == "A"
+  );
 
-    //let nodeMapSet = new Set(nodeMap);
-    let startingNodes = Object.keys(nodeMap).filter(nodeName => [...nodeName][2] == 'A');
+  //console.table(startingNodes);
 
-    //console.table(startingNodes);
+  //let currentNodes = startingNodes;
 
-    let currentNodes = startingNodes;
+  // while (!currentNodes.every(nodeName => ([...nodeName][2] == 'Z'))) {
 
-    while (!currentNodes.every(nodeName => ([...nodeName][2] == 'Z'))) {
+  //     //traversingSteps.push(currentNodes);
+  //     //console.table(currentNodes);
+  //     traversingStepsCount++;
+  //     if (traversingStepsCount % 1000000 == 0) {
+  //         console.log(traversingStepsCount);
+  //     }
 
-        //traversingSteps.push(currentNodes);
-        //console.table(currentNodes);
-        traversingStepsCount++;
-        if (traversingStepsCount % 1000000 == 0) {
-            console.log(traversingStepsCount);
+  //     currentNodes = currentNodes.map(currentNode => {
+  //         //traversingSteps.push(currentNode);
+  //         let node = nodeMap[currentNode];
+
+  //         currentNode = node.left;
+  //         if (navigation[navigationIndex] == 'R') {
+  //             currentNode = node.right;
+  //         }
+
+  //         return currentNode;
+  //     })
+
+  //     navigationIndex++;
+  //     if (navigation.length == navigationIndex) {
+  //         navigationIndex = 0;
+  //     }
+
+  // }
+
+  let FirstZAppears = startingNodes.map((node) => {
+    let currentNode= node;
+    let stepCount=0;
+    while ([...currentNode][2] != 'Z') {
+        //traversingSteps.push(currentNode);
+        stepCount++;
+        let node = nodeMap[currentNode];
+    
+        currentNode = node.left;
+        if (navigation[navigationIndex] == "R") {
+          currentNode = node.right;
         }
-
-        currentNodes = currentNodes.map(currentNode => {
-            //traversingSteps.push(currentNode);
-            let node = nodeMap[currentNode];
-
-            currentNode = node.left;
-            if (navigation[navigationIndex] == 'R') {
-                currentNode = node.right;
-            }
-
-
-
-            return currentNode;
-        })
-
+    
         navigationIndex++;
         if (navigation.length == navigationIndex) {
-            navigationIndex = 0;
+          navigationIndex = 0;
         }
+      }
+      return stepCount;
 
+  });
 
-    }
+  function gcd(a: number, b: number): number {
+    return a ? gcd(b % a, a) : b;
+  }
 
-    console.log(`Star Two : ${traversingSteps.length}`);
+  function lcm(a: number, b: number) {
+    return (a * b) / gcd(a, b);
+  }
+
+  let starTwo = FirstZAppears.reduce(lcm);
+  console.log(`Star Two : ${starTwo}`);
 }
