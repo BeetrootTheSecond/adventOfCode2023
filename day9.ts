1dc083c0332380cf694acb1f6bec50953581957d
aaron <aaronhorrell@hotmail.co.uk>
2023-12-12 00:06:50 +0000

    Day 9 star two Complete

--- day9.ts
+++ day9.ts
@@ -19,7 +19,7 @@ function findSequenceDifferences(sequence: number[]) {
     return differences;
 
 }
-
+let backwardsNumberInSequences: number[] = [];
 
 let nextNumberInSequence = Data.map(line => {
     console.log(line);
@@ -29,31 +29,37 @@ let nextNumberInSequence = Data.map(line => {
     while (!stepDifferences[0].every(x => x == 0)) {
         let nextLevel = findSequenceDifferences(stepDifferences[0]);
         stepDifferences.unshift(nextLevel);
-        console.log(nextLevel);
+        //console.log(nextLevel);
     }
 
-    console.log('Found Zero');
+    //console.log('Found Zero');
 
     //find next number in sequence
     for (let index = 0; index < stepDifferences.length; index++) {
         let lineLength = stepDifferences[index].length;
         let prevousStep = 0;
+        let starTwoPrevousStep = 0;
         if (index != 0) {
             prevousStep = stepDifferences[index - 1][stepDifferences[index - 1].length - 1];
+            starTwoPrevousStep = stepDifferences[index - 1][0];
         }
 
         //let prevousStep = stepDifferences[index - 1][stepDifferences[index - 1].length] || 0;
         stepDifferences[index].push(stepDifferences[index][lineLength - 1] + prevousStep);
+        stepDifferences[index].unshift(stepDifferences[index][0] - starTwoPrevousStep);
         console.log(stepDifferences[index]);
     }
 
     let nextNumber = stepDifferences[stepDifferences.length - 1][stepDifferences[stepDifferences.length - 1].length - 1];
-
+    let prevousNextNumber = stepDifferences[stepDifferences.length - 1][0];
+    backwardsNumberInSequences.push(prevousNextNumber);
     return nextNumber;
 
 })
 
 
 let starOne = nextNumberInSequence.reduce((total, value) => (total + value));
+let starTwo = backwardsNumberInSequences.reduce((total, value) => (total + value));
 
 console.log(`StarOne ${starOne}`);
+console.log(`StarTwo ${starTwo}`);
