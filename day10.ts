25b42185df9235fb1970976cb663efd3d725e363
Aaron Horrell <aaronhorrell@hotmail.co.uk>
2023-12-18 16:49:41 +0000

    Day10 files created and consoleColor added

---
+++ day10.ts
@@ -0,0 +1,13 @@
+import { data, sample } from "./day9Data";
+import { consoleColors } from "./consoleColors";
+
+let sampleMode = false;
+const currentData = sampleMode ? sample : data;
+
+interface tileType{
+    tileChar:string,
+
+}
+
+
+
